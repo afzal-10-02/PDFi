b7c68bdae1ff3de9ec225f7dfe02a4c69c9bb878
afzal-10-02 <your-email@example.com>
2025-07-30 16:29:18 +0530

    added vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 import AppFooter from './assets/Footer.jsx'
 import AppHeader from './assets/Header.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 
 
